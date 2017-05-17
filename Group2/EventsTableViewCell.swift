a7ab5b407c17d043ec1a723c79c6e54531bae331
Obiet Panggrahito <obiet.musicscape@gmail.com>
2017-05-17 13:57:34 +0800

    commit

---
+++ Group2/EventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsTableViewCell.swift
+//  Group2
+//
+//  Created by Obiet Panggrahito on 17/05/2017.
+//  Copyright © 2017 Obiet Panggrahito. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Group2/MyEventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyEventsTableViewCell.swift
+//  Group2
+//
+//  Created by Obiet Panggrahito on 17/05/2017.
+//  Copyright © 2017 Obiet Panggrahito. All rights reserved.
+//
+
+import UIKit
+
+class MyEventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
